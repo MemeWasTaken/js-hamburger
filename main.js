60a19caea40783d6954e87cf81c6b6e64c314002
MemeWasTaken <memelazzari02@gmail.com>
2021-11-22 16:49:42 +0100

    dualism hambMenu

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 // Al primo click:
-    // .fas.fa-bars -> .fas.fa-times
-    // li display inline-block -> block
-// => if value= .fas.fa-bars
+// .fas.fa-bars -> .fas.fa-times
+// => if value= 'fas fa-bars'
+// li display inline-block -> block
 
 // Al secondo click
-    // .fas.fa-times -> .fas.fa-bars
-    // li display block -> inline-block
-// => if value= .fas.fa-times
+// .fas.fa-times -> .fas.fa-bars
+// => if value= 'fas fa-times'
+// li display block -> inline-block
+
+const hambMenu = document.querySelector('.header-right > a');
+console.log(hambMenu);
+const btnIcon = document.querySelector('div > a > i');
+hambMenu.addEventListener('click', function () {
+    const classBtnIcon = btnIcon.className;
+    console.log(classBtnIcon);
+    if(classBtnIcon == 'fas fa-bars') {
+        btnIcon.classList.remove('fa-bars')
+        btnIcon.classList.add('fa-times')
+    } else {
+        btnIcon.classList.remove('fa-times')
+        btnIcon.classList.add('fa-bars')
+    }
+});
