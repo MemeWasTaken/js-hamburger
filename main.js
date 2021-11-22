89ebbd5fc10465c30c8d32ce469195cb28cfda2c
MemeWasTaken <memelazzari02@gmail.com>
2021-11-22 16:13:22 +0100

    problem in words

---
+++ main.js
@@ -0,0 +1,9 @@
+// Al primo click:
+    // .fas.fa-bars -> .fas.fa-times
+    // li display inline-block -> block
+// => if value= .fas.fa-bars
+
+// Al secondo click
+    // .fas.fa-times -> .fas.fa-bars
+    // li display block -> inline-block
+// => if value= .fas.fa-times
