1ef9cb02ce9332af76c7aaa9273eef0e158ac7f1
MemeWasTaken <memelazzari02@gmail.com>
2021-11-22 17:14:04 +0100

    add hambMenuActive

--- main.js
+++ main.js
@@ -7,18 +7,25 @@
 // .fas.fa-times -> .fas.fa-bars
 // => if value= 'fas fa-times'
 // li display block -> inline-block
-
 const hambMenu = document.querySelector('.header-right > a');
 console.log(hambMenu);
+
+const hambMenuActive = document.querySelector('.hamburger-menu');
+
 const btnIcon = document.querySelector('div > a > i');
 hambMenu.addEventListener('click', function () {
     const classBtnIcon = btnIcon.className;
     console.log(classBtnIcon);
     if(classBtnIcon == 'fas fa-bars') {
-        btnIcon.classList.remove('fa-bars')
-        btnIcon.classList.add('fa-times')
+        btnIcon.classList.remove('fa-bars');
+        btnIcon.classList.add('fa-times');
+        hambMenuActive.classList.add('active');
     } else {
-        btnIcon.classList.remove('fa-times')
-        btnIcon.classList.add('fa-bars')
+        btnIcon.classList.remove('fa-times');
+        btnIcon.classList.add('fa-bars');
+        hambMenuActive.classList.remove('active');
     }
+    
+
+    
 });
