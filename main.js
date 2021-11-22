5fdece873bf48924fbf9f9f94e79c245dabc2763
MemeWasTaken <memelazzari02@gmail.com>
2021-11-22 17:21:28 +0100

    finished

--- main.js
+++ main.js
@@ -7,25 +7,22 @@
 // .fas.fa-times -> .fas.fa-bars
 // => if value= 'fas fa-times'
 // li display block -> inline-block
+
 const hambMenu = document.querySelector('.header-right > a');
-console.log(hambMenu);
-
+// console.log(hambMenu);
 const hambMenuActive = document.querySelector('.hamburger-menu');
-
 const btnIcon = document.querySelector('div > a > i');
 hambMenu.addEventListener('click', function () {
     const classBtnIcon = btnIcon.className;
-    console.log(classBtnIcon);
-    if(classBtnIcon == 'fas fa-bars') {
-        btnIcon.classList.remove('fa-bars');
-        btnIcon.classList.add('fa-times');
-        hambMenuActive.classList.add('active');
-    } else {
-        btnIcon.classList.remove('fa-times');
-        btnIcon.classList.add('fa-bars');
-        hambMenuActive.classList.remove('active');
-    }
-    
+    // console.log(classBtnIcon);
+    btnIcon.classList.remove('fa-bars');
+    btnIcon.classList.add('fa-times');
+    hambMenuActive.classList.add('active');
+});
 
-    
+const closeMenu = document.querySelector('.close');
+closeMenu.addEventListener('click', function () {
+    btnIcon.classList.remove('fa-times');
+    btnIcon.classList.add('fa-bars');
+    hambMenuActive.classList.remove('active');
 });
